f11ad73cefcfa1822719e7140ed1e064c0d6281a
Robin <robinkaris7@gmail.com>
2019-07-22 00:55:12 +0300

    this commit will README.md file

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,35 @@
 $('#textarea').attr('rows', 20);
+
+$("#development").click(function() {
+  $("#p1").show();
+
+});
+$("#development").click(function() {
+  $("#p1").hide();
+});
+
+
+
+$("#design").click(function() {
+  $("#p2").show();
+
+});
+$("#design").click(function() {
+  $("#p2").hide();
+});
+
+$("#product").click(function() {
+  $("#p3").show();
+
+});
+$("#product").click(function() {
+  $("#p3").hide();
+});
+$(".contactus").submit(function(event){
+    var name = document.getElementById("name");
+
+    $(".nameform").append(+ name + "we have received your email.we will get back to you soon ")
+
+
+    event.preventDefault();
+});
